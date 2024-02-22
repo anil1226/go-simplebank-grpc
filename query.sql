f8178e517fe0e83dd33853faebf0674ba9153164
Anil Karuturi <anilchowdary1226@gmail.com>
2024-02-21 20:33:29 -0500

    init commit

---
+++ query.sql
@@ -0,0 +1,67 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner, balance,currency
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccount :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE accounts
+  set balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
+
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+  from_account_id,
+  to_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+WHERE 
+    from_account_id = $1 OR
+    to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
+
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id,
+  amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+WHERE account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
